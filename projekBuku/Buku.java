0421b62a8f5ef445c3044cf3ed5329dd25265014
chelsea-hutapeaa <chelseahutapea1212@gmail.com>
2026-09-05 10:42:17 +0700

    Refactor Buku class to use String for genre

--- projekBuku/Buku.java
+++ projekBuku/Buku.java
@@ -1,24 +1,20 @@
-public class Buku{
+public class Buku {
     private String kode;
     private String judul;
-    private GenreBuku genre;
+    private String genre;
 
-    public Buku(String kode, String judul,  GenreBuku genre) {
+    public Buku(String kode, String judul, GenreBBuku genre) {
         this.kode = kode;
         this.judul = judul;
         this.genre = genre;
-    
         genre.tambahBuku(this);
     }
-
     public String getKode() {
         return kode;
     }
-
     public String getJudul() {
         return judul;
     }
-
     public GenreBuku getGenre() {
         return genre;
     }
