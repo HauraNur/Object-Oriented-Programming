ec61fdf618fab7c0e2b1208a4d17e6eb86bfc829
chelsea-hutapeaa <chelseahutapea1212@gmail.com>
2026-09-05 10:33:47 +0700

    Add files via upload

---
+++ projekBuku/Buku.java
@@ -0,0 +1,25 @@
+public class Buku{
+    private String kode;
+    private String judul;
+    private GenreBuku genre;
+
+    public Buku(String kode, String judul,  GenreBuku genre) {
+        this.kode = kode;
+        this.judul = judul;
+        this.genre = genre;
+    
+        genre.tambahBuku(this);
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public GenreBuku getGenre() {
+        return genre;
+    }
+}
